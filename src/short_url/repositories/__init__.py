c6980415cf8386df4d521c8fcca106e728423e4d
Nicolas <nicolas.parucker@hotmail.com>
2026-09-24 09:53:55 +0700

    some more renaming

--- src/short_url/repositories/__init__.py
+++ src/short_url/repositories/__init__.py
@@ -1,13 +1,13 @@
-from short_url.repositories.click_repository import ClickRepository
+from short_url.repositories.click_repository import PostgresClickRepository
 from short_url.repositories.result import Result
-from short_url.repositories.surl_repository import SurlRepository
-from short_url.repositories.url_repository import UrlRepository
-from short_url.repositories.user_repository import UserRepository
+from short_url.repositories.surl_repository import PostgresSurlRepository
+from short_url.repositories.url_repository import PostgresUrlRepository
+from short_url.repositories.user_repository import PostgresUserRepository
 
 __all__ = [
     "Result",
-    "UserRepository",
-    "UrlRepository",
-    "SurlRepository",
-    "ClickRepository",
+    "PostgresUserRepository",
+    "PostgresUrlRepository",
+    "PostgresSurlRepository",
+    "PostgresClickRepository",
 ]
